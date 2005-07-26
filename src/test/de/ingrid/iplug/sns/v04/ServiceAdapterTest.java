7526b07851b4b7d46927eab77e210144108c52e1
hs <hs@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2005-07-26 14:38:28 +0000

    initial commit

---
+++ src/test/de/ingrid/iplug/sns/v04/ServiceAdapterTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 1997-2005 by media style GmbH
+ * 
+ * $Source: DispatcherTest.java,v $
+ */
+package de.ingrid.iplug.sns.v04;
+
+import junit.framework.TestCase;
+
+/**
+ * Test for service-adapter-feature (INGRID-17).
+ * created on 21.07.2005 <p>
+ *
+ * @author hs
+ */
+public class ServiceAdapterTest extends TestCase {
+	
+	public void testCommunicationWithSNS(){
+		fail("INGRID-100 not yet implemented");
+	}
+	
+}
