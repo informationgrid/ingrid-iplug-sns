a5ae59df4b17320e8db1a5e87af98899f23a9783
hs <hs@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2005-08-26 07:39:22 +0000

    o initial commit o sns web service client

---
+++ src/java/de/ingrid/iplug/sns/SNSServiceAdapter.java
@@ -0,0 +1,194 @@
+/*
+ * Copyright (c) 1997-2005 by media style GmbH
+ * 
+ * $Source: DispatcherTest.java,v $
+ */
+package de.ingrid.iplug.sns;
+
+import java.math.BigInteger;
+import java.net.URL;
+import java.rmi.RemoteException;
+
+import com.slb.taxi.webservice.xtm.stubs.FieldsType;
+import com.slb.taxi.webservice.xtm.stubs.SearchType;
+import com.slb.taxi.webservice.xtm.stubs.XTMESoapPortType;
+import com.slb.taxi.webservice.xtm.stubs.XTMserviceLocator;
+import com.slb.taxi.webservice.xtm.stubs._autoClassify;
+import com.slb.taxi.webservice.xtm.stubs._findTopics;
+import com.slb.taxi.webservice.xtm.stubs._getPSI;
+import com.slb.taxi.webservice.xtm.stubs._getTypes;
+import com.slb.taxi.webservice.xtm.stubs._topicMapFragment;
+
+/**
+ * Adapter which provides the access to the sns webservice.  
+ *  
+ * created on 21.07.2005 <p>
+ *
+ * @author hs
+ */
+public class SNSServiceAdapter {
+    private String fUserName = null;
+    private String fPassword = null;
+    private String fLanguage = null;
+    private XTMESoapPortType fXtmSoapPortType = null;
+    
+    /**
+     * Constructs an instance by using the given parameters.  
+     * @param userName Is used for authentication on the webservice. 
+     * @param password Is used for authentication on the webservice.
+     * @param language Is used to specify the preferred language for requests. 
+     * @throws Exception
+     */
+    
+    public SNSServiceAdapter(String userName, String password, String language) throws Exception {
+        this(userName, password, language, null);
+    }
+    
+    /**
+     * Constructs an instance by using the given parameters.  
+     * @param userName Is used for authentication on the webservice. 
+     * @param password Is used for authentication on the webservice.
+     * @param language Is used to specify the preferred language for requests. 
+     * @param url
+     * @throws Exception
+     */
+    public SNSServiceAdapter(String userName, String password, String language, URL url) throws Exception {
+        this.fUserName = userName;
+        this.fPassword = password;
+        this.fLanguage = language;
+        if(url == null){
+            this.fXtmSoapPortType = new XTMserviceLocator().getXTMSoapPort();
+        }
+        else{
+            this.fXtmSoapPortType = new XTMserviceLocator().getXTMSoapPort(url);
+        }
+    }
+    
+    /**
+     * 
+     * @throws Exception
+     */
+    public SNSServiceAdapter() throws Exception{
+        this(null, null, null, null);
+    }
+    
+    /**
+     * Sends a findTopics request by using the underlaying webservice client.<br>
+     * All parameters will passed to the _findTopics request object.
+     * @param queryTerm 
+     * @param path The path is used to qualify the result.
+     * @param searchType Can be one of the provided <code>SearchType</code>s. 
+     * @param fieldsType Can be one of the provided <code>FieldsType</code>s.
+     * @param offset Defines the number of topics to skip.  
+     * @return The response object.
+     * @throws Exception
+     * @see SearchType
+     * @see FieldsType
+     */
+    
+    public _topicMapFragment findTopics(String queryTerm, String path, SearchType searchType, FieldsType fieldsType, long offset) throws Exception{
+        if(queryTerm == null){
+            throw new IllegalArgumentException("QueryTerm can not be null");
+        }
+        if(offset < 0){
+            throw new IllegalArgumentException("Offset can not be lower than 0");
+        }
+        _findTopics topicRequest = new _findTopics();
+        topicRequest.setUser(this.fUserName);
+        topicRequest.setPassword(this.fPassword);
+        topicRequest.setLang(this.fLanguage);
+        if(path != null){
+            topicRequest.setPath(path);
+        }
+        if(searchType != null){
+            topicRequest.setSearchType(searchType);
+        }
+        if(fieldsType != null){
+            topicRequest.setFields(fieldsType);
+        }
+        topicRequest.setOffset(BigInteger.valueOf(offset));
+        topicRequest.setQueryTerm(queryTerm);
+        return this.fXtmSoapPortType.findTopicsOp(topicRequest);
+    }
+    
+    /**
+     * Sends a getPSI request by using the underlaying webservice client.<br>
+     * All parameters will passed to the _getPSI request object.
+     * @param topicID      
+     * @param distance 
+     * @return The response object.
+     * @throws Exception
+     */
+    
+    public _topicMapFragment getPSI(String topicID, int distance) throws Exception{
+        if(topicID == null){
+            throw new IllegalArgumentException("TopicID can not be  null");
+        }
+        if(distance < 0 || distance > 3){
+            throw new IllegalArgumentException("Distance must have a value between 0 and 3");
+        }
+        _getPSI psiRequest = new _getPSI();
+        psiRequest.setUser(this.fUserName);
+        psiRequest.setPassword(this.fPassword);
+        psiRequest.setDistance(BigInteger.valueOf(distance));
+        psiRequest.setId(topicID);
+        return this.fXtmSoapPortType.getPSIOp(psiRequest);
+    }
+    
+    /**
+     * Sends a autoClassify request by using the underlaying webservice client.<br>
+     * All parameters will passed to a _autoClassiy request object. 
+     * @param document The text to analyze.
+     * @param analyzeMaxWords The number of words to analyze. 
+     * @return
+     * @throws Exception
+     */
+    
+    public _topicMapFragment autoClassify(String document, int analyzeMaxWords) throws Exception{
+        if(document == null){
+            throw new IllegalArgumentException("document can not be null");
+        }
+        if(analyzeMaxWords < 0){
+            throw new IllegalArgumentException("AnalyzeMaxWords can not lower than 0");
+        }
+        _autoClassify classifyRequest = new _autoClassify();
+        classifyRequest.setUser(this.fUserName);
+        classifyRequest.setPassword(this.fPassword);
+        classifyRequest.setLang(this.fLanguage);
+        classifyRequest.setDocument(document);
+        classifyRequest.setAnalyzeMaxWords("" + analyzeMaxWords);
+        return this.fXtmSoapPortType.autoClassifyOp(classifyRequest);
+    }
+    
+    /**
+     * 
+     * @return
+     * @throws RemoteException
+     */
+    public _topicMapFragment getTypes() throws RemoteException{
+       _getTypes typeRequest = new _getTypes();
+       typeRequest.setPassword(this.fPassword);
+       typeRequest.setUser(this.fUserName);
+       return this.fXtmSoapPortType.getTypesOp(typeRequest);
+    }
+    
+    /**
+     * @param language
+     */
+    public void setLanguage(String language) {
+        this.fLanguage = language;
+    }
+    /**
+     * @param password
+     */
+    public void setPassword(String password) {
+        this.fPassword = password;
+    }
+    /**
+     * 
+     * @param userName
+     */
+    public void setUserName(String userName) {
+        this.fUserName = userName;
+    }
+}
