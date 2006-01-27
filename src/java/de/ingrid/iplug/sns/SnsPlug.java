f4ee9778f8928ebe64f4ab361370a326a48034bb
ms <ms@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-01-27 10:34:56 +0000

    o set PlugId in hits o first translation from topic to hit

--- src/java/de/ingrid/iplug/sns/SnsPlug.java
+++ src/java/de/ingrid/iplug/sns/SnsPlug.java
@@ -8,6 +8,7 @@ package de.ingrid.iplug.sns;
 
 import de.ingrid.iplug.IPlug;
 import de.ingrid.iplug.PlugDescription;
+import de.ingrid.iplug.sns.utils.Topic;
 import de.ingrid.utils.IngridDocument;
 import de.ingrid.utils.IngridHit;
 import de.ingrid.utils.IngridHits;
@@ -34,11 +35,15 @@ public class SnsPlug implements IPlug {
 
     private int fMaximalAnalyzedWord;
 
+    private String fProviderId;
+
+    private String fPlugId;
+
     /**
      * @throws Exception
      */
     public SnsPlug() throws Exception {
-        // TODO: SHOULD this values come from the PlugDescription?
+        // TODO: SHOULD these values come from the PlugDescription?
         Configuration configuration = new Configuration();
         configuration.load(this.getClass().getResourceAsStream(CONFIGURATIONFILE));
         String username = configuration.get("username", "");
@@ -51,23 +56,57 @@ public class SnsPlug implements IPlug {
     /**
      * @see de.ingrid.iplug.IPlug#search(de.ingrid.utils.query.IngridQuery, int, int)
      */
-    public IngridHits search(IngridQuery query, int start, int lenght) {
+    public IngridHits search(IngridQuery query, int start, int length) {
+        int count = 0;
+        IngridHit[] hits = new IngridHit[0];
+
         if (query.getDataType().equals(IDataTypes.SNS)) {
+            Topic[] topic = new Topic[0];
             String type = (String) query.get(REQUEST_TYPE);
+
+            // FIXME: By TOPIC FROM TOPIC i get the range by the other i must select my range. Is this correct?
             try {
                 if (type.equals(TOPIC_FROM_TERM)) {
-                    this.fSnsController.getTopicsForTerm(getSearchTerm(query), start, lenght);
+                    topic = this.fSnsController.getTopicsForTerm(getSearchTerm(query), start, length);
                 } else if (type.equals(TOPIC_FROM_TEXT)) {
-                    this.fSnsController.getTopicsForText((String) query.getContent(), this.fMaximalAnalyzedWord);
+                    topic = this.fSnsController
+                            .getTopicsForText((String) query.getContent(), this.fMaximalAnalyzedWord);
                 } else if (type.equals(TOPIC_FROM_TOPIC)) {
-                    this.fSnsController.getTopicsForTopic(getSearchTerm(query), lenght);
+                    topic = this.fSnsController.getTopicsForTopic(getSearchTerm(query), length);
                 }
+
+                count = topic.length;
+                int max = 0;
+                final int countMinusStart = topic.length - start;
+                if (countMinusStart >= 0) {
+                    max = Math.min(length, countMinusStart);
+                }
+
+                hits = translateTopicToHit(topic, start, max);
             } catch (Exception e) {
                 // TODO: log/react
             }
         }
 
-        return null;
+        return new IngridHits(this.fProviderId, count, hits);
+    }
+
+    private IngridHit[] translateTopicToHit(Topic[] topic, int start, int max) {
+        IngridHit[] hits = new IngridHit[max];
+        for (int i = start; i < (max + start); i++) {
+            // FIXME: What is the id?
+            final int id = 1;
+            // FIXME: What is the score?
+            final float score = 1;
+            // FIXME: What is the datasource ID?
+            final int dataSourceId = 1;
+
+            IngridHit ingridHit = new IngridHit(this.fProviderId, id, dataSourceId, score);
+            ingridHit.setIPlugId(fPlugId);
+            hits[i - start] = ingridHit;
+        }
+
+        return hits;
     }
 
     private String getSearchTerm(IngridQuery query) {
@@ -80,7 +119,8 @@ public class SnsPlug implements IPlug {
     }
 
     public void configure(PlugDescription plugDescription) throws Exception {
-        // nothing for now
+        this.fProviderId = plugDescription.getIPlugClass() + plugDescription.getOrganisation();
+        this.fPlugId = plugDescription.getPlugId();
     }
 
     public IngridDocument getDetails(IngridHit hit) throws Exception {
