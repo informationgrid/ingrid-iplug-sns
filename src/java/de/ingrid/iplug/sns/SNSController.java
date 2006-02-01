b547a7ac74d8b8419595b496d99ca272be040e64
ms <ms@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-02-01 15:59:41 +0000

    o force rebuild

--- src/java/de/ingrid/iplug/sns/SNSController.java
+++ src/java/de/ingrid/iplug/sns/SNSController.java
@@ -24,6 +24,7 @@ import de.ingrid.iplug.sns.utils.Topic;
  * 
  */
 public class SNSController {
+
     private static final String TEMPORAL_TO_OCCURRENCE = "temporalToOcc";
 
     private static final String TEMPORAL_FROM_OCCURRENCE = "temporalFromOcc";
