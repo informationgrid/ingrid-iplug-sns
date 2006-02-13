fcccd6e8fec13c729a50762c3dc263f4c6b9cd89
ms <ms@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-02-13 13:02:50 +0000

    o provide interface for easy sns data indexing

---
+++ src/test/de/ingrid/iplug/sns/SNSIndexingInterfaceTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 1997-2006 by media style GmbH
+ */
+
+package de.ingrid.iplug.sns;
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ */
+public class SNSIndexingInterfaceTest extends TestCase {
+
+    private SNSIndexingInterface fSnsInterface;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        this.fSnsInterface = new SNSIndexingInterface("ms", "portalu2006", "de");
+    }
+
+    /**
+     * @throws Exception 
+     */
+    public void testGetReferencesToSpace() throws Exception {
+        this.fSnsInterface.getBuzzwords("Döllnitz", 1000);
+
+        final String[] result = this.fSnsInterface.getReferencesToSpace();
+        assertNotNull(result);
+        assertEquals(11, result.length);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testGetReferencesToTime() throws Exception {
+        this.fSnsInterface.getBuzzwords("Tschernobyl Ohio", 1000);
+
+        final String[] result = this.fSnsInterface.getReferencesToTime();
+        assertNotNull(result);
+        assertEquals(3, result.length);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testGetBuzzword() throws Exception {
+        final String[] result = this.fSnsInterface.getBuzzwords("Tschernobyl Ohio", 1000);
+        assertNotNull(result);
+        assertEquals(4, result.length);
+    }
+}
