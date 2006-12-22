a303c059011e49ab00efba78e6d75b1f1a6ce4db
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-12-22 18:00:53 +0000

    - branch for m4.2 release

---
+++ 20061220_release_m4_2/src/test/de/ingrid/iplug/sns/SNSControllerPerformanceTest.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 1997-2005 by media style GmbH
+ * 
+ * $Source: $
+ */
+
+package de.ingrid.iplug.sns;
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ */
+public class SNSControllerPerformanceTest extends TestCase {
+
+    private SNSControllerTest fSNSControllerTest;
+    
+    private SNSIndexingInterfaceTest fSNSIndexingInterfaceTest;
+
+    private static final int fMaxLoops = 50;
+
+    /**
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        this.fSNSIndexingInterfaceTest = new SNSIndexingInterfaceTest();
+        this.fSNSIndexingInterfaceTest.setSNSIndexingInterface(new SNSIndexingInterface("ms", "m3d1asyl3", "de"));
+        
+        this.fSNSControllerTest = new SNSControllerTest();
+        SNSClient snsClient = new SNSClient("ms", "m3d1asyl3", "de");
+        snsClient.setTimeout(180000);
+        this.fSNSControllerTest.setSNSClient(snsClient);
+    }
+
+    /**
+     * @throws Exception
+     * 
+     */
+    public void testTopicsForTerm100() throws Exception {
+        final long start = System.currentTimeMillis();
+        for (int i = 0; i < SNSControllerPerformanceTest.fMaxLoops; i++) {
+            this.fSNSControllerTest.testTopicsForTerm();
+        }
+        final long stop = System.currentTimeMillis();
+
+        final String output = "Time for " + SNSControllerPerformanceTest.fMaxLoops + " calls of testTopicsForTerm(): " + ((stop - start) / 1000) + " ("
+                + (SNSControllerPerformanceTest.fMaxLoops / ((stop - start) / 1000)) + " calls/second)";
+        System.out.println(output);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testGetAssociatedTopics100() throws Exception {
+        final long start = System.currentTimeMillis();
+        for (int i = 0; i < SNSControllerPerformanceTest.fMaxLoops; i++) {
+            this.fSNSControllerTest.testGetAssociatedTopics();
+        }
+        final long stop = System.currentTimeMillis();
+
+        final String output = "Time for " + SNSControllerPerformanceTest.fMaxLoops + " calls of testGetAssociatedTopics(): " + ((stop - start) / 1000) + " ("
+                + (SNSControllerPerformanceTest.fMaxLoops / ((stop - start) / 1000)) + " calls/second)";
+        System.out.println(output);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testGetDocumentRelatedTopics100() throws Exception {
+        final long start = System.currentTimeMillis();
+        for (int i = 0; i < SNSControllerPerformanceTest.fMaxLoops; i++) {
+            this.fSNSControllerTest.testGetDocumentRelatedTopics();
+        }
+        final long stop = System.currentTimeMillis();
+
+        final String output = "Time for " + SNSControllerPerformanceTest.fMaxLoops + " calls of testGetDocumentRelatedTopics(): " + ((stop - start) / 1000) + " ("
+        + (SNSControllerPerformanceTest.fMaxLoops / ((stop - start) / 1000)) + " calls/second)"; 
+        System.out.println(output);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testGetBuzzword100() throws Exception {
+        final long start = System.currentTimeMillis();
+        for (int i = 0; i < SNSControllerPerformanceTest.fMaxLoops; i++) {
+            this.fSNSIndexingInterfaceTest.testGetBuzzword();
+        }
+        final long stop = System.currentTimeMillis();
+
+        final String output = "Time for " + SNSControllerPerformanceTest.fMaxLoops + " calls of testGetBuzzword(): " + ((stop - start) / 1000) + " ("
+                + (SNSControllerPerformanceTest.fMaxLoops / ((stop - start) / 1000)) + " calls/second)";
+        System.out.println(output);
+    }
+}
---
+++ 20061220_release_m4_2/src/test/de/ingrid/iplug/sns/SNSIndexingInterfaceTest.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 1997-2006 by media style GmbH
+ */
+
+package de.ingrid.iplug.sns;
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ */
+public class SNSIndexingInterfaceTest extends TestCase {
+
+    private SNSIndexingInterface fSnsInterface;
+
+    private boolean fToStdout;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        this.fSnsInterface = new SNSIndexingInterface("ms", "m3d1asyl3", "de");
+        this.fSnsInterface.setTimeout(180000);
+        
+        this.fToStdout = true;
+    }
+
+   /**
+    * @param iinterface 
+ * @throws Exception
+    */
+   public void setSNSIndexingInterface(SNSIndexingInterface iinterface) throws Exception {
+       this.fSnsInterface = iinterface;
+   }
+    
+    /**
+     * @throws Exception
+     */
+    public void testGetReferencesToSpace() throws Exception {
+        this.fSnsInterface.getBuzzwords("Halle", 1000, false);
+
+        final Wgs84Box[] result = this.fSnsInterface.getReferencesToSpace();
+        assertNotNull(result);
+        assertEquals(5, result.length);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i].getTopicName());
+            System.out.println("x1:" + result[i].getX1());
+            System.out.println("y1:" + result[i].getY1());
+            System.out.println("x2:" + result[i].getX2());
+            System.out.println("y2:" + result[i].getY2());
+            System.out.println(result[i].getGemeindekennziffer());
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testGetReferencesToSpaceBundesland() throws Exception {
+        this.fSnsInterface.getBuzzwords("Sachsen", 1000, true);
+
+        final Wgs84Box[] result = this.fSnsInterface.getReferencesToSpace();
+        assertNotNull(result);
+        assertEquals(2, result.length);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i].getTopicName());
+            System.out.println(result[i].getX1());
+            System.out.println(result[i].getX2());
+            System.out.println(result[i].getY1());
+            System.out.println(result[i].getY2());
+            System.out.println(result[i].getGemeindekennziffer());
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testGetReferencesToTime() throws Exception {
+        this.fSnsInterface.getBuzzwords("Tschernobyl Ohio", 1000, false);
+
+        final Temporal[] result = this.fSnsInterface.getReferencesToTime();
+        assertNotNull(result);
+        assertEquals(4, result.length);
+        System.out.println(result[0].getAt());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testGetBuzzword() throws Exception {
+        String[] result = null;
+        final String words = "In diesem Jahr können sich kleine und mittlere Unternehmen bis zum 15. "
+                + "August 2006 bewerben. Eine aus Vertretern von Wissenschaft, Wirtschaft und mittelständischen "
+                + "Anwenderunternehmen besetzte Jury wird bis zu drei Bewerber aus den Kategorien E-Business, Breitband und "
+                + "Mobilität auswählen und mit Preisen in Höhe von je 25.000 Euro auszeichnen. Die Preisverleihung findet im "
+                + "Rahmen des 2. Deutschen ITK-Mittelstandstages im November 2006 statt Für die Verwendung der "
+                + "Ein-Ausgabe-Klassen muss das Package java.io importiert werden Wir haben bereits gelernt, wie die Ein- und "
+                + "Ausgabe in Graphischen User-Interfaces programmiert wird. Nun wollen wir uns auch damit beschäftigen, wie wir "
+                + "Daten von Dateien einlesen und in Dateien speichern können. Wir haben bereits gelernt, wie die Ein- und "
+                + "Ausgabe in Graphischen User-Interfaces programmiert wird.";
+        final long start = System.currentTimeMillis();
+        try {
+            result = this.fSnsInterface.getBuzzwords(words, 1000, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+        final long end = System.currentTimeMillis();
+
+        if (this.fToStdout) {
+            final String output = "Time for getting all buzzwords: " + ((end - start) / 1000) + " s";
+            System.out.println(output);
+        }
+
+        assertNotNull(result);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testGetBuzzwordNotExistent() throws Exception {
+        final String[] result = this.fSnsInterface.getBuzzwords("blabla", 1000, false);
+        assertNotNull(result);
+        assertEquals(0, result.length);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public void testGetReferencesNotExistent() throws Exception {
+        this.fSnsInterface.getBuzzwords("blabla", 1000, false);
+
+        final Temporal[] result = this.fSnsInterface.getReferencesToTime();
+        assertNotNull(result);
+        assertEquals(0, result.length);
+    }
+}
