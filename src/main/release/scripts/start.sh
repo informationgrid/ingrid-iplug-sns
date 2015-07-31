80ee952a71821f068e7c1b7a7207470e262f847a
Toan Tran <khanh.toan.tran@wemove.com>
2015-07-31 14:37:28 +0200

    Change order of INGRID_HOME and INGRID_OPTS. (https://redmine.wemove.com/issues/729)

--- src/main/release/scripts/start.sh
+++ src/main/release/scripts/start.sh
@@ -181,7 +181,7 @@ startIplug()
   fi
 
   export CLASSPATH="$CLASSPATH"
-  INGRID_OPTS="$INGRID_OPTS -Dingrid_home=$INGRID_HOME"
+  INGRID_OPTS="-Dingrid_home=$INGRID_HOME $INGRID_OPTS"
   CLASS=de.ingrid.iplug.PlugServer
 
   # run it
